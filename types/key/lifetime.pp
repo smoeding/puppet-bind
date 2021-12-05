ba590368a0ef0f6a07e40920bc99873d0df42487
Stefan Möding <stm@kill-9.net>
2021-12-05 17:41:52 +0100

    Update pattern for type Bind::Key::Lifetime

--- types/key/lifetime.pp
+++ types/key/lifetime.pp
@@ -1,2 +1,2 @@
 # Type to match allowed values for the key lifetime
-type Bind::Key::Lifetime = Variant[Bind::Duration,Enum['unlimited']]
+type Bind::Key::Lifetime =Pattern[/^(-?)P(?=\d|T\d)(?:(\d+)Y)?(?:(\d+)M)?(?:(\d+)([DW]))?(?:T(?:(\d+)H)?(?:(\d+)M)?(?:(\d+(?:\.\d+)?)S)?)?$/, /\Aunlimited\Z/]
