5b950708a34b43b844cdb160a8ab76d15d15f0f7
Stefan Möding <stm@kill-9.net>
2021-12-28 17:02:20 +0100

    Add line break

--- types/key/lifetime.pp
+++ types/key/lifetime.pp
@@ -1,2 +1,5 @@
 # Type to match allowed values for the key lifetime
-type Bind::Key::Lifetime =Pattern[/^(-?)P(?=\d|T\d)(?:(\d+)Y)?(?:(\d+)M)?(?:(\d+)([DW]))?(?:T(?:(\d+)H)?(?:(\d+)M)?(?:(\d+(?:\.\d+)?)S)?)?$/, /\Aunlimited\Z/]
+type Bind::Key::Lifetime = Pattern[
+  /^(-?)P(?=\d|T\d)(?:(\d+)Y)?(?:(\d+)M)?(?:(\d+)([DW]))?(?:T(?:(\d+)H)?(?:(\d+)M)?(?:(\d+(?:\.\d+)?)S)?)?$/,
+  /\Aunlimited\Z/
+]
