6bb13ede9ced3a356cd89987fa9db01345967e8d
Stefan Möding <stm@kill-9.net>
2022-10-20 07:59:42 +0200

    Use canonical service name instead of alias

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -319,11 +319,20 @@ describe 'bind' do
         #
 
         it {
-          is_expected.to contain_service('bind')
-            .with_ensure('running')
-            .with_enable(true)
-            .with_name('bind9')
-            .with_restart('/usr/sbin/rndc reconfig')
+          case "#{facts[:os]['name']}-#{facts[:os]['release']['major']}"
+          when 'Debian-10'
+            is_expected.to contain_service('bind')
+              .with_ensure('running')
+              .with_enable(true)
+              .with_name('bind9')
+              .with_restart('/usr/sbin/rndc reconfig')
+          else
+            is_expected.to contain_service('bind')
+              .with_ensure('running')
+              .with_enable(true)
+              .with_name('named')
+              .with_restart('/usr/sbin/rndc reconfig')
+          end
         }
       end
 
