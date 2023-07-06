bf75604c402efd8a721469ccc6c4f40c42ecf421
Stefan Möding <stm@kill-9.net>
2023-07-06 13:17:33 +0200

    Update to PDK 3.0.0

--- spec/unit/facter/named_version_spec.rb
+++ spec/unit/facter/named_version_spec.rb
@@ -8,7 +8,7 @@ describe 'named_version', type: :fact do
   context 'when named version is 9.11.5' do
     before(:each) do
       allow(Facter::Core::Execution).to receive(:execute)
-        .with('named -V', on_fail: nil, timeout: 3)
+        .with('named -V', { on_fail: nil, timeout: 3 })
         .and_return('BIND 9.11.5-P4-5.1+deb10u2-Debian (Extended Support Version) <id:998753c>')
     end
 
@@ -20,7 +20,7 @@ describe 'named_version', type: :fact do
   context 'when named version is 9.16.1' do
     before(:each) do
       allow(Facter::Core::Execution).to receive(:execute)
-        .with('named -V', on_fail: nil, timeout: 3)
+        .with('named -V', { on_fail: nil, timeout: 3 })
         .and_return('BIND 9.16.1-Ubuntu (Stable Release) <id:d497c32>')
     end
 
